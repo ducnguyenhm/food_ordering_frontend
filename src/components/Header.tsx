6bbbc86fc46183270a7f6880a96e191b6af107d4
ducnguyenhm <nguyenhmduc1502@gmail.com>
2024-09-11 17:10:09 -0400

    add Features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
 import MainNav from "./MainNav";
+import header from "../assets/header.png"
 
 const Header = () => {
     return (
-      <div className="border-b-2 border-b-orange-500 py-6">
+      <div className="border-b-2 bg-yellow-300 border-b-black py-6">
         <div className="container mx-auto flex justify-between items-center">
             <Link
             to="/"
-            className="text-3xl font-bold tracking-tight text-orange-500"
+            className="text-3xl font-bold tracking-tight text-black"
             >
-            DepauwOrder.com
+              <img src={header} className="w-full max-h-[80px] object-cover" />
             </Link>
             <div className = "md:hidden">
                 <MobileNav/>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,13 +11,13 @@ const HomePage = () => {
             pathname:`/search/${searchFormValues.searchQuery}`,
 
         });
-    };
+    }
 
 
     return(
         <div className="flex flex-col gap-12">
             <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-                <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+                <h1 className="text-5xl font-bold tracking-tight text-black">
                 Tuck into a takeway today
                 </h1>
                 <span className="text-xl">Food is just a click away!</span>
